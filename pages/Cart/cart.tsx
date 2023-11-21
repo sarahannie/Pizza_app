24d3db9366c77c1ed4fdec2d53b4113efa822630
sarahannie <sarahndianekwu@gmail.com>
2023-11-21 15:14:03 +0100

    cart desktop screen done

--- pages/Cart/cart.tsx
+++ pages/Cart/cart.tsx
@@ -7,15 +7,17 @@ import style from './cart.module.css'
 const Cart = () => {
   return (
     <>
-        <div className='flex'>
+        <div className={style.container}>
             <div>
             <> 
  
-<div className="w-full overflow-x-auto">
+<div className={`w-full overflow-x-auto ${style.table}`}>
   <table className="w-full text-left border-collapse rounded w-overflow-x-auto " cellSpacing="0">
     <tbody>
       <tr className="border-b border-slate-300">
         <th scope="col" className="h-12 px-6 text-sm font-medium stroke-slate-700 text-slate-700 ">Product</th>
+        <th scope="col" className="h-12 px-6 text-sm font-medium stroke-slate-700 text-slate-700 ">Name</th>
+        <th scope="col" className="h-12 px-6 text-sm font-medium stroke-slate-700 text-slate-700 ">Extra</th>
         <th scope="col" className="h-12 px-6 text-sm font-medium stroke-slate-700 text-slate-700 ">Price</th>
         <th scope="col" className="h-12 px-6 text-sm font-medium stroke-slate-700 text-slate-700 ">Quantity</th>
         <th scope="col" className="h-12 px-6 text-sm font-medium stroke-slate-700 text-slate-700 ">Total</th>
@@ -24,12 +26,20 @@ const Cart = () => {
       <tr className="border-b border-slate-200">
         <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
           <div className="flex items-center gap-4">
-            <div className="w-30 h-19 bg-slate-200 rounded-sm">
+            <div className="w-30 h-19  rounded-sm">
                 <Image src="/image/pizza-1.png" width={120} height={100} alt='Vacter Image' />
             </div>
+            
+            </div>
+        </td>
+        <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
             <div>
               <h3 className="text-sm font-medium">Shrimp Pizza</h3>
             </div>
+        </td>
+        <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
+            <div>
+              <h3 className="text-sm font-medium"></h3>
             </div>
         </td>
         <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
@@ -50,12 +60,20 @@ const Cart = () => {
       <tr className="border-b border-slate-200">
         <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
           <div className="flex items-center gap-4">
-            <div className="w-30 h-19 bg-slate-200 rounded-sm">
+            <div className="w-30 h-19  rounded-sm">
                 <Image src="/image/pizza-1.png" width={120} height={100} alt='Vacter Image' />
             </div>
+            
+            </div>
+        </td>
+        <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
             <div>
               <h3 className="text-sm font-medium">Shrimp Pizza</h3>
             </div>
+        </td>
+        <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
+            <div>
+              <h3 className="text-sm font-medium"></h3>
             </div>
         </td>
         <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
@@ -76,12 +94,20 @@ const Cart = () => {
       <tr className="border-b border-slate-200">
         <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
           <div className="flex items-center gap-4">
-            <div className="w-30 h-19 bg-slate-200 rounded-sm">
+            <div className="w-30 h-19  rounded-sm">
                 <Image src="/image/pizza-1.png" width={120} height={100} alt='Vacter Image' />
             </div>
+            
+            </div>
+        </td>
+        <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
             <div>
               <h3 className="text-sm font-medium">Shrimp Pizza</h3>
             </div>
+        </td>
+        <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
+            <div>
+              <h3 className="text-sm font-medium"></h3>
             </div>
         </td>
         <td className="h-12 px-6 text-sm transition duration-300 border-slate-200 stroke-slate-500 text-slate-500 ">
@@ -99,6 +125,7 @@ const Cart = () => {
           </div>
         </td>
       </tr>
+      
     </tbody>
   </table>
 </div>
@@ -106,18 +133,18 @@ const Cart = () => {
 </div>
             <div className={style.cartContainer}>
                <div>
-                <h2 className='text-2xl px-6 mb-4 mt-4 text-slate-900'>Cart Totals</h2>
+                <h2 className='text-2xl px-6 mb-4 mt-4 text-white-900'>Cart Totals</h2>
                 </div>
                 <div>
-                <div className='flex justify-between text-slate-500 py-2'>  
+                <div className='flex justify-between text-white-500 py-2'>  
                     <h3>Subtotal</h3>
                     <h3>$35.00</h3>
                 </div> 
-                <div className='flex justify-between text-slate-500 text-slate-500 py-2'> 
+                <div className='flex justify-between text-white-500  py-2'> 
                       <h3>Shipping</h3>
                       <h3>$5.00</h3>
                 </div> 
-                <div className='flex justify-between text-slate-500 py-2'>  
+                <div className='flex justify-between text-white-500 py-2'>  
                    <h3> Order Total</h3>
                    <h3>$40.00</h3>
                  </div> 
