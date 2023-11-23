b95ccf753d11bcde6f70d7f27aad052b1acca9b7
sarahannie <sarahndianekwu@gmail.com>
2023-11-23 16:39:49 +0100

    done with logout route

---
+++ app/api/user/logout/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    try{
+        const response = NextResponse.json({
+            message: 'Logout successful',
+            sucees: true
+        })
+        response.cookies.set("token", "",{
+            httpOnly: true,
+            expires: new Date(0)
+        })
+    }catch(error:any){
+        return NextResponse.json({error:error.message}, {status:500})
+        console.log(error.message)
+    }
+}
