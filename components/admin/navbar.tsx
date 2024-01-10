53ce82fd4ffba18acba5d3c58ce05ac08dbda4ad
sarahannie <sarahndianekwu@gmail.com>
2024-01-10 13:21:15 +0100

    search  bar implemented

--- components/admin/navbar.tsx
+++ components/admin/navbar.tsx
@@ -187,12 +187,12 @@ export const Navbar = () => {
       </div>
       {
         show && (
-          <div className="inline-flex flex-col justify-center relative text-gray-500 mt-4 mx-auto">
+          <div className="flex justify-center relative text-gray-500 mt-4 ">
           <div className="relative">
               <input type="text"
               value={searchTerm}
               onChange={(e) => handleInputChange(e.target.value)}
-               className="p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-[#d43b49] focus:border-transparent" placeholder="search..."  />
+               className="p-2 pl-8 rounded border w-[100%] border-[#d43b49]  focus:bg-white focus:outline-none focus:ring-2 focus:ring-[#d43b49] focus:border-transparent" placeholder="search..."  />
               <svg className="w-4 h-4 absolute left-2.5 top-3.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
               </svg>
